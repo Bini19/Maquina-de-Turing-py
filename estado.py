0feb5b6cb04f0a76b908381f5c21d4abc2f42b00
Guilherme Bini <77159678+Bini19@users.noreply.github.com>
2021-04-02 02:05:28 -0300

    Add files via upload

---
+++ estado.py
@@ -0,0 +1,18 @@
+class estado():
+    def __init__(self,nome,transicoes=[],is_final=False):
+        self.nome = nome
+        self.transicoes = transicoes
+        self.is_final = is_final
+        
+    def __str__(self):
+        return self.nome
+    
+    def get_transicao(self,simbolo):
+        for transicao in self.transicoes:
+            if transicao[0] is simbolo:
+                    return transicao
+                
+        return None
+        
+    def get_nome(self):
+        return self.nome
